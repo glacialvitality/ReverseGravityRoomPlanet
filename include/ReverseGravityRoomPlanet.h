751f466e7cb51a8dc99736e862499a5873350733
GlacialVitality <glacialvitality@gmail.com>
2026-05-17 14:24:00 -0400

    port code to Syati

--- include/ReverseGravityRoomPlanet.h
+++ include/ReverseGravityRoomPlanet.h
@@ -2,3 +2,19 @@
 
 #include "syati.h"
 
+
+class ReverseGravityRoomPlanet : public MapObjActor {
+public:
+    ReverseGravityRoomPlanet(const char*);
+
+    virtual ~ReverseGravityRoomPlanet();
+    virtual void init(const JMapInfoIter&);
+    virtual void initCaseUseSwitchA(const MapObjActorInitInfo&);
+
+    void exeDownStart();
+    void exeDownWait();
+    void exeUpStart();
+    void exeUpWait();
+    void startSwitchOn();
+    void startSwitchOff();
+};
--- source/ReverseGravityRoomPlanet.cpp
+++ source/ReverseGravityRoomPlanet.cpp
@@ -1,2 +1,62 @@
 #include "ReverseGravityRoomPlanet.h"
 
+namespace NrvReverseGravityRoomPlanet {
+    FULL_NERVE(ReverseGravityRoomPlanetNrvDownStart, ReverseGravityRoomPlanet, DownStart);
+    FULL_NERVE(ReverseGravityRoomPlanetNrvDownWait, ReverseGravityRoomPlanet, DownWait);
+    FULL_NERVE(ReverseGravityRoomPlanetNrvUpStart, ReverseGravityRoomPlanet, UpStart);
+    FULL_NERVE(ReverseGravityRoomPlanetNrvUpWait, ReverseGravityRoomPlanet, UpWait);
+};  // namespace NrvReverseGravityRoomPlanet
+
+ReverseGravityRoomPlanet::ReverseGravityRoomPlanet(const char* pName) : MapObjActor(pName) {}
+
+void ReverseGravityRoomPlanet::init(const JMapInfoIter& rIter) {
+    MapObjActor::init(rIter);
+    MapObjActorInitInfo info;
+    info.setupHioNode("惑星");
+    info.setupDefaultPos();
+    info.setupConnectToScene();
+    info.setupEffect(0, false);
+    info.setupFarClipping(-1.0f);
+    info.setupNerve(&NrvReverseGravityRoomPlanet::ReverseGravityRoomPlanetNrvDownWait::sInstance);
+    initialize(rIter, info);
+}
+
+void ReverseGravityRoomPlanet::exeDownStart() {
+    if (MR::isFirstStep(this)) {
+        MR::startBck(this, "ChangeRed", 0);
+    }
+
+    if (MR::isBckStopped(this)) {
+        setNerve(&NrvReverseGravityRoomPlanet::ReverseGravityRoomPlanetNrvDownWait::sInstance);
+    }
+}
+
+void ReverseGravityRoomPlanet::exeDownWait() {}
+
+void ReverseGravityRoomPlanet::exeUpStart() {
+    if (MR::isFirstStep(this)) {
+        MR::startBck(this, "ChangeBlue", 0);
+    }
+
+    if (MR::isBckStopped(this)) {
+        setNerve(&NrvReverseGravityRoomPlanet::ReverseGravityRoomPlanetNrvUpWait::sInstance);
+    }
+}
+
+void ReverseGravityRoomPlanet::exeUpWait() {}
+
+void ReverseGravityRoomPlanet::initCaseUseSwitchA(const MapObjActorInitInfo& rIter) {
+    void (ReverseGravityRoomPlanet::*startOff)(void) = &ReverseGravityRoomPlanet::startSwitchOff;
+    void (ReverseGravityRoomPlanet::*startOn)(void) = &ReverseGravityRoomPlanet::startSwitchOn;
+    MR::listenStageSwitchOnOffA(this, MR::Functor(this, startOff), MR::Functor(this, startOn));
+}
+
+void ReverseGravityRoomPlanet::startSwitchOn() {
+    setNerve(&NrvReverseGravityRoomPlanet::ReverseGravityRoomPlanetNrvUpStart::sInstance);
+}
+
+void ReverseGravityRoomPlanet::startSwitchOff() {
+    setNerve(&NrvReverseGravityRoomPlanet::ReverseGravityRoomPlanetNrvDownStart::sInstance);
+}
+
+ReverseGravityRoomPlanet::~ReverseGravityRoomPlanet() {}
